d53706611742bda5b35e978a22c836275604d121
Robin Schneider <robin.schneider@codecamp-n.com>
2025-02-19 14:29:59 +0100

    rename package

---
+++ src/main/java/com/codecamp/spring/app/template/api/controller/ContractController.java
@@ -0,0 +1,21 @@
+package com.codecamp.spring.app.template.api.controller;
+
+import com.codecamp.spring.app.template.api.model.ContractResponse;
+import com.codecamp.spring.app.template.business.service.ContractService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ContractController implements ContractApi {
+
+    private final ContractService contractService;
+
+    @Override
+    public ResponseEntity<ContractResponse> getContract(String name) {
+        ContractResponse response = contractService.findContract(name);
+        return ResponseEntity.ok(response);
+    }
+
+}
